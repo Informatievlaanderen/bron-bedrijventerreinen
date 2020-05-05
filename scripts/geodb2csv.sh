874879d0e598590c138a16f1143c40bae3f0f157
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2020-05-05 15:22:39 +0000

    voeg gdal processing toe

--- scripts/geodb2csv.sh
+++ scripts/geodb2csv.sh
@@ -4,4 +4,13 @@
 FILE=$1
 OUTFILE=$2
 
-ogr2ogr -f CSV -lco GEOMETRY=AS_WKT -lco ENCODING=UTF-8 -skipfailures ${OUTFILE}.csv ${FILE}.gpkg 
+WORKDIR=/tmp/workspace
+
+#ogr2ogr -f CSV -lco GEOMETRY=AS_WKT -lco ENCODING=UTF-8 -skipfailures ${OUTFILE}.csv ${FILE}.gpkg 
+
+cp ${FILE} ${WORKDIR}
+pushd ${WORKDIR}
+unzip ${FILE}
+ogr2ogr -f CSV -lco GEOMETRY=AS_WKT -lco ENCODING=UTF-8 -skipfailures ${OUTFILE}.csv ${FILE}
+
+popd
